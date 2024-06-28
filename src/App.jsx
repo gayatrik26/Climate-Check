9d882c280db2989b97c32976e6e0c91e1376bf8e
Gayatri <gayatrik22803@gmail.com>
2024-06-28 19:59:59 +0530

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useState , useEffect} from 'react';
 import './index.css';
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import Temprature from './components/Temprature';
 import Forecast from './components/Forecast';
+import getWeatherData from './services/WeatherServices';
 
 function App() {
+  // const getWeather = async () =>{
+  //   const data = await getWeatherData("weather" , "berlin");
+  //   console.log(data);    
+  // }
+  // getWeather();
+
+  const [weatherData, setWeatherData] = useState(null);
+  const getWeather = async () => {
+    const data = await getWeatherData("weather", "berlin");
+    setWeatherData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+
   return (
     <div className='mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700 text-white'>
-      <TopButtons/>
-      <Inputs/>
-      <TimeAndLocation/>
-      <Temprature/>
-      <Forecast/>
-      <Forecast/>
+      <TopButtons />
+      <Inputs />
+      <TimeAndLocation />
+      <Temprature />
+      <Forecast />
+      <Forecast />
     </div>
   )
 }
--- src/services/WeatherServices.js
+++ src/services/WeatherServices.js
@@ -1,1 +1,37 @@
+// const ApiKey = import.meta.env.VITE_APP_API_KEY;
+// const baseUrl = "https://api.openweathermap.org/data/2.5/";
+
+// const getWeatherData = async (infotype , city) =>{
+//     try {
+//         const url = `https://api.openweathermap.org/data/2.5/weather${infotype}?q=${city}&appid=${this.ApiKey}`;
+//         // const url = new URL(baseUrl + infotype);
+//         // url.search = new URLSearchParams({...searchPara , appid : ApiKey});
+//         const response =  await fetch(url);
+//         const weatherData = await response.json();
+//         return weatherData;
+//         // return fetch(url).then((res) => res.json()).then(data => data);  
+//     } catch (error) {
+//         console.log("errrrrrrrrrr" , error)
+//     }
+    
+
+// };
+
+// export default getWeatherData;
+
+
 const ApiKey = import.meta.env.VITE_APP_API_KEY;
+const baseUrl = "https://api.openweathermap.org/data/2.5/";
+
+const getWeatherData = async (infotype, city) => {
+    try {
+        const url = `${baseUrl}${infotype}?q=${city}&appid=${ApiKey}`;
+        const response = await fetch(url);
+        const weatherData = await response.json();
+        return weatherData;
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
+};
+
+export default getWeatherData;
